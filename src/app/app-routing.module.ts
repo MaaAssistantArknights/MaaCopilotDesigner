3911597376fe808bccdf885090ce927eaeb4bddc
Shawnsdaddy <wu2xx@dukes.jmu.edu>
2022-06-28 23:11:07 -0700

    fix 增加Hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './copilot/componments/home/home.component';
 
-const routes: Routes = [{path:'',redirectTo:'maacopilotdesigner',pathMatch:'full'},
-{path:'/maacopilotdesigner',component: HomeComponent}];
+const routes: Routes = [
+{ path: '**', redirectTo: 'maacopilotdesigner', pathMatch: 'full' },
+{ path: '', redirectTo: 'maacopilotdesigner', pathMatch: 'full' },
+{ path: 'maacopilotdesigner', component: HomeComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{ useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
